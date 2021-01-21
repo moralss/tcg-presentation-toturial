a4ebef883919fe64d4ab5d56e2f15d3c2f01764b
Moral <moral@devson.co.za>
2021-01-21 12:22:09 +0200

    added radio button example

--- covid-app/src/App.js
+++ covid-app/src/App.js
@@ -3,63 +3,116 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
-      name: "", surname: "", status: "", patients: []
+      name: "", surname: "", status: "",
+      patients: [],
     }
   }
 
 
-  handleChange(e) {
-    console.log(e.target.name, e.target.value)
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-
-  saveStatus(e) {
+  saveStatus (e) {
     this.setState({ status: e.target.value })
   }
 
+  handleInputChange (e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
-  savePatientDetails(e) {
+  savePatientDetails (e) {
     e.preventDefault();
     const { name, surname, status, patients } = this.state;
-    this.setState({ patients: [...patients, { name, surname, status }] })
+    let starterStatus = [{ name: "positive", status: false },
+    { name: "negative", status: false },
+    { name: "recovery", status: false }]
+
+    const filteredStatus = starterStatus.filter(item => item.name !== status)
+    const newPatient = { name, surname, status: [...filteredStatus, { name: status, status: true }] }
+    this.setState({ patients: [...patients, newPatient] })
+    this.setState({ name: "", surname: "", status: "" })
     console.log(this.state)
   }
 
-  render() {
-    const { name, patients, surname } = this.state
+  handleChange = (e) => {
+    const { name, surname, status, patients } = this.state;
+    const findIndex = patients.findIndex(item => item.name == e.target.name);
+    var patientsList = [...patients]
+    for (var j in patientsList[findIndex].status) {
+      patientsList[findIndex].status[j].status = false
+    }
+
+    let filterList = patients[findIndex].status.filter(item => item.name != e.target.value)
+    var result = { status: [...filterList, { name: e.target.value, status: true }] }
+    console.log("filterList", result)
+    patients[findIndex].status = result.status
+    this.setState({ patients: patientsList })
+
+  }
+
+  render () {
+    const { name, patients, surname, list } = this.state
     return (
       <div className="App">
         <h1> COVID APP</h1>
         <form onSubmit={(e) => this.savePatientDetails(e)}>
-          <input type="text" onChange={(e) => this.handleChange(e)} name="name" placeholder="name" value={name} />
-          <input type="text" onChange={(e) => this.handleChange(e)} name="surname" placeholder="surname" value={surname} />
+          <input type="text" onChange={(e) => this.handleInputChange(e)} name="name" placeholder="name" value={name} />
+          <input type="text" onChange={(e) => this.handleInputChange(e)} name="surname" placeholder="surname" value={surname} />
           <div style={{ display: "inline" }} onChange={(e) => this.saveStatus(e)}>
             <input name="status" type="radio" value="positive" /> Positive
             <input name="status" type="radio" value="negative" /> Negative
+            <input name="status" type="radio" value="recovery" /> recovery
           </div>
           <input type="submit" />
         </form>
+
         <div style={{ display: "grid", gridTemplateColumns: "repeat(2 , 1fr)" }}>
           <div>
             <h1> patients who are positive </h1>
-            {patients.filter(patient => patient.status == "positive").map(patient => <div style={{ display: "flex", margin: "5rem" }}>
-              <li style={{ listStyle: "none" }}> {patient.name}</li>
-              <li style={{ listStyle: "none" }}> {patient.surname}</li>
-            </div>)}
+            {patients.filter(patient => patient.status.filter(item => item.name == "positive" && item.status == true)[0]).
+              map((patient, index) => <div style={{ display: "flex", margin: "5rem" }}>
+                <li style={{ listStyle: "none" }}> {patient.name}</li>
+                <li style={{ listStyle: "none" }}> {patient.surname}</li>
+                <div style={{ display: "inline" }}>
+                  {patient.status.map(item =>
+                    <div>
+                      <input name={patient.name} index={index} type="radio" onChange={(e) => this.handleChange(e)} checked={item.status} value={item.name} /> {item.name}
+                    </div>
+                  )}
+                </div>
+              </div>)}
           </div>
           <div>
             <h1> patients who are negative </h1>
-            {patients.filter(patient => patient.status == "negative").map(patient => <div style={{ display: "flex", margin: "5rem" }}>
-              <li style={{ listStyle: "none" }}> {patient.name}</li>
-              <li style={{ listStyle: "none" }}> {patient.surname}</li>
-            </div>)}
+            {patients.filter(patient => patient.status.filter(item => item.name == "negative" && item.status == true)[0]).
+              map((patient, index) => <div style={{ display: "flex", margin: "5rem" }}>
+                <li style={{ listStyle: "none" }}> {patient.name}  </li>
+                <li style={{ listStyle: "none" }}> {patient.surname}  </li>
+                <div style={{ display: "inline" }}>
+                  {patient.status.map(item =>
+                    <div>
+                      <input name={patient.name} index={index} type="radio" onChange={(e) => this.handleChange(e)} checked={item.status} value={item.name} /> {item.name}
+                    </div>
+                  )}
+                </div>
+              </div>)}
+          </div>
+          <div>
+            <h1> patients who are recovery</h1>
+            {patients.filter(patient => patient.status.filter(item => item.name == "recovery" && item.status == true)[0]).
+              map((patient, index) => <div style={{ display: "flex", margin: "5rem" }}>
+                <li style={{ listStyle: "none" }}> {patient.name}</li>
+                <li style={{ listStyle: "none" }}> {patient.surname}</li>
+                <div style={{ display: "inline" }} >
+                  {patient.status.map(item =>
+                    <div>
+                      <input name={patient.name} index={index} type="radio" onChange={(e) => this.handleChange(e)} checked={item.status} value={item.name} /> {item.name}
+                    </div>
+                  )}
+                </div>
+              </div>)}
           </div>
         </div>
-        total of negative patients : {patients.filter(patient => patient.status == "negative").length}
       </div>
     );
   }
